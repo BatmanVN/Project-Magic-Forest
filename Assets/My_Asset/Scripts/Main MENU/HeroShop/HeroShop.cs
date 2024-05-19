c7178ac82bd1c8144fe500d297dcee772e660b1e
VuLinh1611 <vumanhlinh16111995@gmail.com>
2024-05-19 14:49:06 +0700

    a

---
+++ Assets/My_Asset/Scripts/Main MENU/HeroShop/HeroShop.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HeroShop : MonoBehaviour
+{
+    [SerializeField] private GameObject[] heroNumber;
+    [SerializeField] private GameObject[] indexBar;
+    [SerializeField] private GameObject[] noteIndex;
+    [SerializeField] private GameObject xButton;
+    [SerializeField] private GameObject noteButton;
+    [SerializeField] private GameObject buttonNext;
+    [SerializeField] private GameObject buttonBack;
+    [SerializeField] private int number;
+    private void NumberIndex()
+    {
+        if (number >= 2 && number < 0)
+        {
+            return;
+        }
+        if (number > 0 && number < 3)
+        {
+            buttonBack.SetActive(true);
+        }
+        else
+        {
+            buttonBack.SetActive(false);
+        }
+        if (number >= 0 && number < 2)
+        {
+            buttonNext.SetActive(true);
+        }
+        else
+        {
+            buttonNext.SetActive(false);
+        }
+    }
+    public void NextHero()
+    {
+        if (number < 2)
+        {
+            number++;
+        }
+        NumberIndex();
+        for (int i = 0; i < heroNumber.Length; i++)
+        {
+            if (heroNumber[i] == heroNumber[number])
+            {
+                heroNumber[number].SetActive(true);
+                if (number >= heroNumber.Length)
+                {
+                    return;
+                }
+            }
+            else
+            {
+                heroNumber[i].SetActive(false);
+            }
+        }
+    }
+    public void BackHero()
+    {
+        if (number > 0)
+        {
+            number--;
+        }
+        NumberIndex();
+        for (int i = 0;i < heroNumber.Length;i++)
+        {
+            if (heroNumber[i] == heroNumber[number])
+            {
+                heroNumber[number].SetActive(true);
+            }
+            else
+            {
+                heroNumber[i].SetActive(false);
+            }
+        }
+    }
+    public void EnableNote()
+    {
+        for(int i = 0;i < indexBar.Length ; i++)
+        {
+            indexBar[i].SetActive(false);
+            noteButton.SetActive(false);
+            for(int j = 0; j < noteIndex.Length; j++)
+            {
+                noteIndex[j].SetActive(true);
+                xButton.SetActive(true);
+            }
+        }
+    }
+    public void DissableNote()
+    {
+        for (int i = 0; i < noteIndex.Length; i++)
+        {
+            noteIndex[i].SetActive(false);
+            xButton.SetActive(false);
+            for (int j = 0; j < indexBar.Length; j++)
+            {
+                indexBar[j].SetActive(true);
+                noteButton.SetActive(true);
+            }
+        }
+    }
+    public void BackMenu(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+    private void Update()
+    {
+        
+    }
+
+}
--- Assets/My_Asset/Scripts/SaveFile/Button_another/Button_Menu.cs
+++ Assets/My_Asset/Scripts/SaveFile/Button_another/Button_Menu.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Button_Menu : MonoBehaviour
 {
     [SerializeField] private GameObject[] adsBar;
-    
+    [SerializeField] private string sceneName;
     public void EnableAdsBar(int number)
     {
         for (int i = 0; i < adsBar.Length; i++)
@@ -13,6 +14,10 @@ public class Button_Menu : MonoBehaviour
             if (adsBar[i] == adsBar[number])
             {
                 adsBar[number].SetActive(true);
+                if (adsBar[i] == adsBar[4])
+                {
+                    SceneManager.LoadScene(sceneName);
+                }
             }
             else
             {
