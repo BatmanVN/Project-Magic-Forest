eda1b579744e6cd77db78f62f7c6e239dbf6a54c
LongNguyen <nguyenhoanglong112001@gmail.com>
2024-06-03 21:20:55 +0700

    clas 6.3

---
+++ Assets/My_Asset/Scripts/Sign In.cs
@@ -0,0 +1,31 @@
+using GooglePlayGames.BasicApi;
+using GooglePlayGames;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignIn : MonoBehaviour
+{
+    public bool connectedToGooglePlay;
+     void Awake()
+    {
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+    }
+     void Start()
+    {
+        LoginGooglePlay();
+    }
+    void LoginGooglePlay()
+    {
+        PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+    }
+    void ProcessAuthentication(SignInStatus status)
+    {
+        if (status == SignInStatus.Success)
+        {
+            connectedToGooglePlay = true;
+        }
+        else connectedToGooglePlay = false;
+    }
+}
