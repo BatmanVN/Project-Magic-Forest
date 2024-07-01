c8b1c055ce4cd0abbc24e29d7030ababbc0d9195
VuLinh <vumanhlinh16111995@gmail.com>
2024-07-01 14:08:34 +0700

    Try Lean.Pool Plugin

--- Assets/My_Asset/Scripts/Monster/LeafMonster/LeafAttck.cs
+++ Assets/My_Asset/Scripts/Monster/LeafMonster/LeafAttck.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Lean.Pool;
 
 public class LeafAttck : MonoBehaviour
 {
     [SerializeField] private Transform mouthLeaf;
     [SerializeField] private GameObject bulletLeaf;
     [SerializeField] private int delay;
+    //[SerializeField] private LeanGameObjectPool bulletPool;
     private void Awake()
     {
         Leafattack();
     }
     private void Leafattack()
     {
-            Instantiate(bulletLeaf, mouthLeaf.position, Quaternion.identity);
+        Instantiate(bulletLeaf, mouthLeaf.position, Quaternion.identity);
+        //bulletPool.Spawn();
     }
     private IEnumerator Delay()
     {
